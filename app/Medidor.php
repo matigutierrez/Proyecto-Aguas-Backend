eaf61f06a2240b176a15eeffd80556572ccaca07
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2017-11-19 14:52:38 -0300

    Add Model/Controller Medidor

--- app/Medidor.php
+++ app/Medidor.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medidor extends Model
 {
-    //
+    protected $table = 'medidor';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['num_medidor', 'marca_medidor', 'lectura_inicial', 'vivienda_id', 'estado_medidor_id'];
+
+    public function estadoMedidor() {
+    	return $this->belongsTo('App\EstadoMedidor');
+    }
+
+    public function vivienda() {
+    	return $this->belongsTo('App\Vivienda');
+    }
+
+    public function boletas() {
+    	return $this->hasMany('App\BoletaEmitida');
+    }
+
+    public $timestamps = false;
 }
