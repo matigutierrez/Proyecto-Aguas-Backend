7177c930e38ef78a952298ac0951acfa9cc8661a
Matias Gutierrez <m.gutierrez06@ufromail.cl>
2017-11-22 16:01:44 -0300

    Add Model/Controller BoletaEmitida

--- app/BoletaEmitida.php
+++ app/BoletaEmitida.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class BoletaEmitida extends Model
 {
-    //
+    protected $table = 'boleta_emitida';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['nro_boleta', 'nombre_cliente', 'domicilio', 'saldo', 'monto_pagar', 'lectura_anterior', 'lectura_actual', 'consumo', 'subsidio', 'total', 'fecha_pago', 'cargo_fijo', 'alcantarillado', 'multa_reunion', 'multa_corte', 'multa_adulteracion', 'total_abono', 'paga_boleta', 'medidor_id'];
+
+    public function medidor() {
+    	return $this->belongsTo('App\Medidor');
+    }
+
+    public function abonosBoletas() {
+    	return $this->hasMany('App\AbonoMedidor');
+    }
+
+    public $timestamps = false;
 }
