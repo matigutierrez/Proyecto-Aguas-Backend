5ad32f5e3d592dcdbd7e3b18fbe53f74e70dd0f3
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-01-31 18:44:31 -0300

    Cliente nulable

--- database/migrations/2017_12_29_182855_create_usuario_table.php
+++ database/migrations/2017_12_29_182855_create_usuario_table.php
@@ -18,7 +18,7 @@ class CreateUsuarioTable extends Migration
             $table->string('nombre_usu',20)->unique();
             $table->string('password');
             $table->boolean('superadmin');
-            $table->integer('cliente_id')->unsigned();
+            $table->integer('cliente_id')->unsigned()->nullable();
             $table->foreign('cliente_id')->references('id')->on('cliente')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('comite_id')->unsigned()->nullable();
             $table->foreign('comite_id')->references('id')->on('comite')->onDelete('cascade')->onUpdate('cascade');
