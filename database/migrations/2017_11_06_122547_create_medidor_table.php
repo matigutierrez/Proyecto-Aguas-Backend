d251757fdf5ddb7ffc8c4a45fe7b8b234e9917d2
Matías Hermosilla <matiasahermosilla@gmail.com>
2018-03-10 19:24:59 -0300

    Número de medidor puede ser nulo

--- database/migrations/2017_11_06_122547_create_medidor_table.php
+++ database/migrations/2017_11_06_122547_create_medidor_table.php
@@ -15,14 +15,13 @@ class CreateMedidorTable extends Migration
     {
         Schema::create('medidor', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('num_medidor',20);
+            $table->string('num_medidor',20)->nullable();
             $table->string('marca_medidor',20);
             $table->timestamp('fecha_registro');
             $table->integer('lectura_inicial');
             $table->integer('vivienda_id')->unsigned();
             $table->integer('estado_medidor_id')->unsigned();
             $table->integer('comite_id')->unsigned();
-            $table->unique('num_medidor');
             $table->foreign('vivienda_id')->references('id')->on('vivienda')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('estado_medidor_id')->references('id')->on('estado_medidor')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('comite_id')->references('id')->on('comite')->onDelete('cascade')->onUpdate('cascade');
